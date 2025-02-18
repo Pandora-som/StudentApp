950752fbdf1de38f8297084b1af6c8cbeacb32bf
Mari <marisan@mail.ru>
2025-02-18 11:26:20 +0500

    сделала возможность редактирования в админ панеле

--- student/admin.py
+++ student/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Students, Groups
+
+admin.site.register(Students)
+admin.site.register(Groups)
--- student/models.py
+++ student/models.py
@@ -2,10 +2,16 @@ from django.db import models
 
 class Groups(models.Model):
     name_group = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name_group
     
 class Students(models.Model):
     number_group = models.ForeignKey(Groups, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     secondname = models.CharField(max_length=100)
     thirdname = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
     
